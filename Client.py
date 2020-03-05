7c4e3538443d5708461a98a1e45dd02eacc8d2bc
Cass <qwerto227@gmail.com>
2020-03-05 15:35:50 +0000

    Added better time representation to restruant display clientside

--- Client.py
+++ Client.py
@@ -2,6 +2,21 @@ import Pyro4
 import sys
 import math
 
+
+def round_sig(x, sig=2):
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
+
+
+def rep_time(secs):
+    mins = int(math.ceil(secs / 60) / 10) * 10
+    hours = int(mins / 60)
+    if hours >= 1:
+        mins = mins % 60
+        return f"{hours} hour{'s' if hours >= 2 else ''}{f' and {mins} minutes' if mins > 0 else ''}"
+    else:
+        return f'{mins} minutes'
+
+
 def getRestruants():
     print("Please wait for a moment while we contact the server for you...")
     global restruants
@@ -13,11 +28,12 @@ def getRestruants():
         print(f"{i + 1}: \'{restruants[i]}\'")
     print()
 
+
 def displayRestruant(restruant):
     print(f"You have selected {restruant}!")
     print("Please wait a moment while we contact the server and find out more about your choice!")
 
-    #Run the two functions concurrently to minimise the time it takes
+    # Run the two functions concurrently to minimise the time it takes
     dist = Pyro4.Future(Interface.getdistance)(address, postcode, restruant)
     menu = Pyro4.Future(Interface.getmenuitems)(restruant)
     dist.wait()
@@ -30,15 +46,18 @@ def displayRestruant(restruant):
     else:
         print(f"Welcome to {restruant}!")
         if not dist[0]:
-            print(f"There was an error finding how far away you are from {restruant}, make sure you are using a valid address.")
+            print(
+                f"There was an error finding how far away you are from {restruant}, make sure you are using a valid address.")
         else:
-            print(f"{restruant} is located only {dist[0]} miles away by car, which should take our drivers about {int(math.ceil((dist[1]/60)/10))*10} minutes to traverse!")
+            print(
+                f"{restruant} is located only {round_sig(int(dist[0]))} miles away by car, which should take our drivers about {rep_time(dist[1])} to traverse!")
         while True:
             print("This restruant has the following menu items avaliable: ")
             for i in range(len(menu)):
                 item = menu[i]
-                print(f"{i+1}: {item[0]}, £{item[1]} ({', '.join(item[2])})")
-            print("Please enter the number of an item you would like to add to your basket or \"BACK\" to return to main menu")
+                print(f"{i + 1}: {item[0]}, £{item[1]} ({', '.join(item[2])})")
+            print(
+                "Please enter the number of an item you would like to add to your basket or \"BACK\" to return to main menu")
             inp = input("Input: ")
             if inp == "BACK":
                 return False
@@ -47,13 +66,14 @@ def displayRestruant(restruant):
                 if inp > len(menu) or inp < 1:
                     print(f"{inp} is not a valid restruant, please enter a number between 1 and {len(restruants)}")
                 else:
-                    print(f"Adding {menu[inp-1][0]} to your basket!")
+                    print(f"Adding {menu[inp - 1][0]} to your basket!")
                     global basket
-                    basket.append(list(menu[inp-1])+[restruant])
+                    basket.append(list(menu[inp - 1]) + [restruant])
                     print("Now returning to menu...")
                     print("         ---")
             except ValueError:
                 print(f"{inp} is not a valid input, please try again...")
+
 
 def displayCheckout(total):
     global basket
@@ -65,14 +85,16 @@ def displayCheckout(total):
         print("Type \"CONFIRM\" to confirm your purchase or \"BACK\" To return to main menu")
         inp = input("Input: ")
         if inp == "CONFIRM":
+            name = input("Confirming order, please enter your full name: ")
             print("Great! Making order now...")
-            if Interface.makeorder(f"{address}, {postcode}", basket):
+            if Interface.makeorder(name, f"{address}, {postcode}", basket):
                 print("Order successfully made! Returning you to main menu.")
                 orders.append((f"{address}, {postcode}", basket))
             else:
                 print("Oh dear, the order failed! I swear this hasn't happened before! Uh, just, um, can we try again?")
         elif inp == "BACK":
             return False
+
 
 def displayBasket():
     global basket
@@ -83,11 +105,12 @@ def displayBasket():
         print("Your basket contains the following items!")
         total = 0.0
         for i in range(len(basket)):
-            print(f"{i+1} - {basket[i][0]}, £{basket[i][1]} (From {basket[i][3]})")
+            print(f"{i + 1} - {basket[i][0]}, £{basket[i][1]} (From {basket[i][3]})")
             total += basket[i][1]
         print(f"This comes to a total of £{total}")
         print()
-        print("Please enter a number to remove an item from your basket, type \"BACK\" to return to main menu, or type \"CHECKOUT\" to go to checkout with your current basket")
+        print(
+            "Please enter a number to remove an item from your basket, type \"BACK\" to return to main menu, or type \"CHECKOUT\" to go to checkout with your current basket")
         inp = input("Input: ")
         try:
             inp = int(inp)
@@ -95,7 +118,7 @@ def displayBasket():
                 print(f"{inp} is not a valid item, please enter a number between 1 and {len(basket)}")
                 break
             else:
-                del basket[inp-1]
+                del basket[inp - 1]
         except ValueError:
             if inp == "BACK":
                 return False
@@ -112,13 +135,14 @@ def displayOrders():
         print("Come back once you have ordered some delicious food!")
         print()
 
+
 Interface = Pyro4.Proxy("PYRONAME:front.interface")  # use name server object lookup uri shortcut
 
 print("                                   Welcome to Just Hungry!")
 print("-The premiere food delivery service for command line entheusiasts who love installing python modules!-")
 print()
-address = "35 The Moor Melbourn" #input("To start with, please input your address, not including postcode: ")
-postcode = "SG8 6ED" #input("Now input your postcode: ")
+address = "35 The Moor Melbourn"  # input("To start with, please input your address, not including postcode: ")
+postcode = "SG8 6ED"  # input("Now input your postcode: ")
 restruants = []
 basket = []
 orders = []
@@ -128,7 +152,8 @@ while True:
     print()
     print("-- MAIN MENU --")
     print("The following options are now avaliable: ")
-    print(f"Input a number between 1 and {len(restruants)} in order to select the restruant listed above and add items to your basket")
+    print(
+        f"Input a number between 1 and {len(restruants)} in order to select the restruant listed above and add items to your basket")
     print("Input \"RELIST\" to get the list of restruants from the server again")
     print("Input \"REENTER\" to re-enter your address information")
     print("INPUT \"BASKET\" to view your current basket or make an order")
@@ -142,7 +167,7 @@ while True:
             print(f"{inp} is not a valid restruant, please enter a number between 1 and {len(restruants)}")
             break
         else:
-            displayRestruant(restruants[inp-1])
+            displayRestruant(restruants[inp - 1])
     except ValueError:
         if inp == "RELIST":
             getRestruants()
@@ -162,6 +187,5 @@ while True:
             print("Now exiting program...")
             sys.exit()
         else:
-            print(f"Uh oh! \"{inp}\" is not a valid option, returning you to main menu, next time, make better choices!")
-
-
+            print(
+                f"Uh oh! \"{inp}\" is not a valid option, returning you to main menu, next time, make better choices!")
