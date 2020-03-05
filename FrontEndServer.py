ebf55817bf2799a06e5323311233adc5203d226a
Cass <qwerto227@gmail.com>
2020-03-05 15:35:17 +0000

    Added saferun and updateinterfaces to Frontend, checking which backends are working

--- FrontEndServer.py
+++ FrontEndServer.py
@@ -1,9 +1,45 @@
 import Pyro4
-import asyncio
+import Pyro4.errors
+import sys
 import requests
 import json
 
-Interface = Pyro4.Proxy("PYRONAME:back.interface")  # use name server object lookup uri shortcut
+UPDATE_ATTEMPTS = 5  # How many times to attempt to run a function and updateInterfaces before giving up
+
+
+# Test all interfaces to see which one is working
+# use name server object lookup uri shortcut
+def updateInterfaces(brokenProxy=None):
+    global interfacenames, workinginterfaces, currInterface
+    if brokenProxy and len(workinginterfaces) > 1:
+        workinginterfaces.remove(brokenProxy)
+        currInterface = workinginterfaces[0]
+        return True
+
+    workinginterfaces = []
+    for name in interfacenames:
+        try:
+            interface = Pyro4.Proxy(name)
+            interface._pyroBind()
+            interface.ping()
+            if not currInterface:
+                currInterface = interface
+            workinginterfaces.append(interface)
+        except Pyro4.errors.CommunicationError as err:
+            print(f"Interface: {name} is not working, error: {err}")
+        except Pyro4.errors.NamingError:
+            print(f"Interface: {name} is not recognised by the nameserver, likely is not running")
+        except AttributeError:
+            print(f"Ping was not recognised as a valid method, interface is incorrect")
+
+    if not workinginterfaces:
+        print("No interfaces are working! Closing frontend...")
+        sys.exit()
+    else:
+        print(f"The following interfaces are working: {workinginterfaces}")
+        if currInterface == brokenProxy:
+            print("It would appear that the proxy this function was called on was working, trying again")
+            print("This should not happen, and if it does, we are all pretty screwed")
 
 
 def distanceAPIcall(address1, address2):
@@ -22,20 +58,28 @@ def distanceAPIcall(address1, address2):
         return resource['travelDistance'], resource['travelDurationTraffic']
 
 
-#print(getdistance("12 Percy Square, Durham, DH1 3PZ", "86 Fuckyou Fuckenu, Fuckslyvania, Fuck, 123 123"))
+# print(getdistance("12 Percy Square, Durham, DH1 3PZ", "86 Fuckyou Fuckenu, Fuckslyvania, Fuck, 123 123"))
+def safeRun(func, args):
+    for i in range(UPDATE_ATTEMPTS):
+        try:
+            return func(*args)
+        except ConnectionError:
+            updateInterfaces(currInterface)
+
 
 
 @Pyro4.expose
 class ClientInterface:
     def getrestruants(self, address, postcode):
-        return Interface.getrestruants(address, postcode)
+        return safeRun(currInterface.getrestruants, (address, postcode))
+
 
     def getmenuitems(self, restruant):
-        return Interface.getmenuitems(restruant)
+        return safeRun(currInterface.getmenuitems, (restruant, ))
 
     def getdistance(self, address, postcode, restruant):
         try:
-            restruantaddr = Interface.getaddress(restruant)
+            restruantaddr = safeRun(currInterface.getaddress, (restruant, ))
             return distanceAPIcall(f"{address}, {postcode}", restruantaddr)
         except Exception as err:
             if err == "Distance API Timed Out" or err == "Failed to find route":
@@ -44,11 +88,20 @@ class ClientInterface:
                 print(err)
                 return False, "Unknown Error"
 
-    def makeorder(self, fulladdress, basket):
+    def makeorder(self, name, fulladdress, basket):
         try:
+            safeRun(currInterface.makeorder, (name, fulladdress, basket))
             return True
-        except:
+        except Exception:
             return False
+
+
+interfacenames = ["PYRONAME:back.interface.alpha",
+                  "PYRONAME:back.interface.beta",
+                  "PYRONAME:back.interface.gamma"]
+workinginterfaces = []
+currInterface = None
+updateInterfaces()
 
 daemon = Pyro4.Daemon()  # make a Pyro daemon
 ns = Pyro4.locateNS()  # find the name server
